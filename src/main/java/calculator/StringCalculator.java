6acd4e23c202f1f0b0c548ff930a898ec7eb8969
Swapnil Mane <maneswap1@gmail.com>
2021-10-17 00:24:45 +0530

    allow to add method unknown amount of nos completed succesfully

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,17 @@ class StringCalculator {
     public void add(String input) {
     	if(input.length() == 0) {
     		return;
+    		
+    		else{
+    			String delimiter = ",";
+    			if(text.matches("//(.*)\n(.*)")){
+    				delimiter = Character.toString(text.charAt(2));
+    				text = text.substring(4);
+    			}
+    			
+    			String numList[] = splitNumbers(text, delimiter + "|\n");
+    			return sum(numList);
+    		}	
     }
 
 }
